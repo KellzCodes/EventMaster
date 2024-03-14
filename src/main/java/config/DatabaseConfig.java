fb88438f04d16a0d266dbbb65cdb3f6229b9c96d
Kelli <kellidavis320@gmail.com>
2024-03-14 00:20:38 -0400

    Refactor DatabaseConfig for Direct Connection Configuration
    - Updated DatabaseConfig.java to hard-code database connection details, addressing input stream null error when running from command line.
    - Ensures reliable database connection setup by directly specifying JDBC URL, username, and password.

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -1,19 +1,19 @@
 package config;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 /**
  * Utility class for managing database connections.
  */
 public class DatabaseConfig {
 
-    static InputStream inputStream = DatabaseConfig.class.getClassLoader().getResourceAsStream("db.properties");
-    static Properties properties = new Properties();
+    // Database URL
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/eventmaster";
+    // Database credentials
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "password";
 
     /**
      * Attempts to establish a connection to the database.
@@ -21,24 +21,13 @@ public class DatabaseConfig {
      * @throws SQLException If a database access error occurs or the URL is null.
      */
     public static Connection getConnection() throws SQLException {
-
-        String url = null;
-        String user = null;
-        String password = null;
         try {
-            properties.load(inputStream);
-            String driver = properties.getProperty("jdbc.driver");
-            url = properties.getProperty("jdbc.url");
-            user = properties.getProperty("jdbc.user");
-            password = properties.getProperty("jdbc.password");
             // Ensure the JDBC driver is loaded
-            Class.forName(driver);
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
-        return DriverManager.getConnection(url, user, password);
+        return DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
     }
 }
 
